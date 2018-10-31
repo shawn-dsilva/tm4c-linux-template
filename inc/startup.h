360ed3cc341be6500ac464ac3c9478eaa90cc9b2
Jariel Joash Gojar <jariel.gojar@gmail.com>
2018-10-31 23:17:37 +0530

    created gpio.h

--- inc/startup.h
+++ inc/startup.h
@@ -41,27 +41,6 @@ typedef union {
     element_t isr;   //all ISRs use this type
     void *stack_top; //pointer to top of the stack
 } vector_table_t;
-//GPIO Definitions
-
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define PF4                     (*((volatile unsigned long *)0x40025040))
-#define PF3                     (*((volatile unsigned long *)0x40025020))
-#define PF2                     (*((volatile unsigned long *)0x40025010))
-#define PF1                     (*((volatile unsigned long *)0x40025008))
-#define PF0                     (*((volatile unsigned long *)0x40025004))
-#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
-#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
-#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
-#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 
 void PortF_Init(void){ volatile unsigned long delay;
   SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
