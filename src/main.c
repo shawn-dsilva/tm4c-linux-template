e8fd603638ef155674f8942ddd71963e82a501b3
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2019-02-18 15:23:51 +0530

    RGB blinky added with slow blinks

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@
 unsigned long Led;
 
 void Delay(void){unsigned long volatile time;
-  time = 200000;
+  time = 800000;
   while(time){
 		time--;
   }
@@ -34,12 +34,17 @@ void Delay(void){unsigned long volatile time;
 int main(void){
   PortF_Init();
   while(1){
-   if(PF4 == 0x0){
-    Led = GPIO_PORTF_DATA_R;   // read value in PORTF DATA register
-    Led = Led^0x02;            // reverse value of LED
+   //if(PF4 == 0x0){
+    Led = 0x02;            // reverse value of LED
     GPIO_PORTF_DATA_R = Led;   // write value to PORTF DATA register,toggle led
     Delay();
-    }
+    Led = 0x04;            // reverse value of LED
+    GPIO_PORTF_DATA_R = Led;   // write value to PORTF DATA register,toggle led
+    Delay();
+    Led = 0x08;            // reverse value of LED
+    GPIO_PORTF_DATA_R = Led;   // write value to PORTF DATA register,toggle led
+    Delay();
+   // }
   }
 }
 
