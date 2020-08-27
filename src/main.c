0b99b3844d676f2f5386aa2923b20e485b3a7559
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-08-27 02:19:14 +0200

    code simplification, remove redundant macros from main

--- src/main.c
+++ src/main.c
@@ -1,40 +1,22 @@
-
 //main.c
 //Toggles the Red LED of TM4C Launchpad when SW1(PF4) is pushed and held
 //PF4 is negative logic,i.e 0 is on and non-zero is off
-#include"gpio.h"
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define PF4                     (*((volatile unsigned long *)0x40025040))
-#define PF3                     (*((volatile unsigned long *)0x40025020))
-#define PF2                     (*((volatile unsigned long *)0x40025010))
-#define PF1                     (*((volatile unsigned long *)0x40025008))
-#define PF0                     (*((volatile unsigned long *)0x40025004))
-#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
-#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
-#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
-#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#include "gpio.h"
 
 unsigned long Led;
 
-void Delay(void){unsigned long volatile time;
+void Delay(void) {
+  unsigned long volatile time;
   time = 800000;
   while(time){
 		time--;
   }
 }
-int main(void){
+
+int main(void) {
   PortF_Init();
   while(1){
-   //if(PF4 == 0x0){
+    //if(PF4 == 0x0){
     Led = 0x02;            // reverse value of LED
     GPIO_PORTF_DATA_R = Led;   // write value to PORTF DATA register,toggle led
     Delay();
@@ -47,7 +29,7 @@ int main(void){
     /*Led = 0x0A;            // reverse value of LED
     GPIO_PORTF_DATA_R = Led;   // write value to PORTF DATA register,toggle led
     Delay();*/
-   // }
+    // }
   }
 }
 
